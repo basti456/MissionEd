62d4feb30b389fea8b6988bab4725652a8db9b4d
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-08-02 12:45:53 +0530

    MyFirebase

--- lib/MyFirebaseMessaging.dart
+++ lib/MyFirebaseMessaging.dart
@@ -1,43 +1,89 @@
-import 'dart:convert';
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<void> sendPostNotification(notificationTitle, sendersName, message,
-    notificationType, String topic) async {
-  final data = jsonEncode(<String,dynamic>{
-    "notification": {
-      "body": "$message",
-      "title": "$notificationTitle",
-    },
-    "priority": "high",
-    "data":<String, String> {
-      "click_action": "FLUTTER_NOTIFICATION_CLICK",
-      "id": "1",
-      "status": "done",
-      "type": "$notificationType",
-      "sendersUserId": "$sendersName",
-    },
-    "to": "$topic",
-  });
+class SendNotification{
+  Future<void> sendPostNotification(notificationTitle, sendersName, message,
+      notificationType, String topic) async {
+    final data = jsonEncode(<String,dynamic>{
+      "notification": {
+        "body": "$message",
+        "title": "$notificationTitle",
+      },
+      "priority": "high",
+      "data":<String, String> {
+        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+        "id": "1",
+        "status": "done",
+        "type": "$notificationType",
+        "sendersUserId": "$sendersName",
+      },
+      "to": "$topic",
+    });
 
-  final headers =<String, String> {
-    'content-type': 'application/json',
-    'Authorization':
-        'key=AAAAsEWVmyY:APA91bHec56YMfUXYPopN_96kVpDIS8tjbITbo7qfccYDijakzeU0x2-worqN9dvE8ddrhUZUViLh7XgRGLhOO9INHxD0rmPBun7BtFrheBfN_GTedISH-LYQy34yAlmLmQxh6KgWW5S'
-  };
+    final headers =<String, String> {
+      'content-type': 'application/json',
+      'Authorization':
+      'key=AAAA6cFyXKs:APA91bGjKAz_5qu4oacqwcrvL_7--D2buLUGPQrVcbTG8y5TtolqCbbZUrlj5Fa8lQ5MgIzFJcD94w-cZbibJdJ9aRr7Y7ClTZgqMtZxv2nKwxIkxyB6sbvJoPDjXPuBXY8eOgFczOUM'
+    };
 
-  final url = 'https://fcm.googleapis.com/fcm/send';
-  try {
-    final response =
-        await http.post(Uri.parse(url), headers: headers, body: data);
+    final url = 'https://fcm.googleapis.com/fcm/send';
+    try {
+      final response =
+      await http.post(Uri.parse(url), headers: headers, body: data);
 
-    if (response.statusCode == 200) {
-      print('message sent');
-    } else {
-      print('notification sending failed');
+      if (response.statusCode == 200) {
+        print('message sent');
+      } else {
+        print('notification sending failed');
+      }
+    } catch (e) {
+      debugPrint('exception $e');
     }
-  } catch (e) {
-    debugPrint('exception $e');
+  }
+  Future<void> sendDtoDNotification(
+      notificationTitle, message, notificationType,  var tokens, String s) async {
+    // debugPrint('token : $token');
+
+    final data =jsonEncode(<String,dynamic> {
+      "notification": {
+
+        "body": "$message",
+        "title": "$notificationTitle",
+      },
+      "priority": "high",
+      "data":<String, String> {
+        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+        "id": "1",
+        "status": "done",
+        "type": "$notificationType",
+
+      },
+      "to":'c3e92foqTr66cEKCDia2Ei:APA91bH3V3A_E1_9MKYIGH_WFWvUF2Oiaf__zXpCPgQ7dRRtCIzCTQhz70-zF5FVIl4FFyN6F-BdmH1nGJOcdKfk25YuHtDBe5u_f1G98o5pqk2JAW-VlHJEqsVUFTFaFgyLQP_NBvBj',
+
+    });
+    // print(topic);
+    final headers = <String, String>{
+      'content-type': 'application/json',
+      'Authorization':
+      'key=AAAA6cFyXKs:APA91bGjKAz_5qu4oacqwcrvL_7--D2buLUGPQrVcbTG8y5TtolqCbbZUrlj5Fa8lQ5MgIzFJcD94w-cZbibJdJ9aRr7Y7ClTZgqMtZxv2nKwxIkxyB6sbvJoPDjXPuBXY8eOgFczOUM'
+    };
+
+
+    final url = 'https://fcm.googleapis.com/fcm/send';
+    try {
+      final response = await http.post(Uri.parse(url), headers: headers, body: data);
+
+      if (response.statusCode == 200) {
+        print('message sent');
+      } else {
+        print('notification sending failed');
+        // on failure do sth
+      }
+    } catch (e) {
+      print(e);
+      // debugPrint('exception $e');
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,80 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:mission_ed/authenticate/authenticate_firebase.dart';
 import 'package:provider/provider.dart';
 import 'google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+const AndroidNotificationChannel channel= AndroidNotificationChannel(
+'high_importance_channel', // id
+'High Importance Notifications', // title
+'This channel is used for important notifications.', // description
+importance: Importance.high,
+  playSound: true,
+);
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin=FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
+  await Firebase.initializeApp();
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+  ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification=message.notification;
+      AndroidNotification androidNotification=message.notification?.android;
+      if(notification!=null&& androidNotification!=null){
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                playSound: true,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
+      }
+    });
+   /* FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification=message.notification;
+      AndroidNotification androidNotification=message.notification?.android;
+      if(notification!=null&& androidNotification!=null){
+       showDialog(context: context, builder: (_){
+         return AlertDialog(
+           title:Text(notification.title) ,
+         );
+       });
+      }
+    });*/
+  }
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
