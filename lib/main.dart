30f19a61c02cc81a664d81b398e5d3c518903f13
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-08-03 13:15:12 +0100

    changes made

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomeScreen(),
+        home: AuthenticateFirebase(),
       ),
     );
   }
